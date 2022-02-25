5d010aa49fcb934002c6a1d164d481e33dd92892
Quy Tran <quy.tran@dbm.plus>
2022-02-25 11:07:17 +0000

    update rabbitmq config restructure shell scripts

--- _devtools/run-n-test.sh
+++ _devtools/run-n-test.sh
@@ -1,46 +1,12 @@
 # export DOCKER_HOST=ssh://ubuntu@192.168.1.98
 
-export MONGO_INITDB_ROOT_USERNAME=admin
-export MONGO_INITDB_ROOT_PASSWORD=admin123
-export MONGO_HOST=mongodb
-export MONGO_PORT=27017
-export MONGO_USER=admin
-export MONGO_PASS=admin123
+bash ./_devtools/subtask/01-run-base-service.sh
 
-export RABBITMQ_USER=admin
-export RABBITMQ_PASS=admin
-export RABBITMQ_HOST=rabbitmq
-export RABBITMQ_PROTOCOL=amqp
-export RABBITMQ_PORT=5672
-
-PROJECT=simple-shopping
-# Deploy core services
-docker-compose -p $PROJECT -f ./docker-compose-test.yml up -d
-
-# echo 'We need to wait for 30s before running test case'
-sleep 15
-
-RABBITMQ_CONTAINER_ID=$(echo $PROJECT)_rabbitmq_1
-docker exec -ti $RABBITMQ_CONTAINER_ID rabbitmqctl add_user admin admin
-docker exec -ti $RABBITMQ_CONTAINER_ID rabbitmqctl set_user_tags admin administrator
-docker exec -ti $RABBITMQ_CONTAINER_ID rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"s
-
+echo 'We need to wait for 20s before running test case...'
+sleep 20
 
 # Build product image and test
-bash ./_devtools/build-product-svc.sh
+bash ./_devtools/subtask/02-build-product-svc.sh
 
-NETWORK=$(echo $PROJECT)_shopping_net
-docker run \
-  --rm \
-  -it \
-  --name product_svc_testing \
-  --network=$NETWORK \
-  -e MONGO_HOST=$MONGO_HOST \
-  -e MONGO_PORT=$MONGO_PORT \
-  -e MONGO_USER=$MONGO_USER \
-  -e MONGO_PASS=$MONGO_PASS \
-  -e RABBITMQ_HOST=$RABBITMQ_HOST \
-  -e RABBITMQ_PROTOCOL=$RABBITMQ_PROTOCOL \
-  -e RABBITMQ_USER=$RABBITMQ_USER \
-  -e RABBITMQ_PASS=$RABBITMQ_PASS \
-  product_svc:testing npm run test
+# run test
+bash ./_devtools/subtask/03-run-test.sh
