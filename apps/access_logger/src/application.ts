0ea86955eb55b30ba6bed73355d58482c9eb7dfa
Quy Tran <tuanquynet@gmail.com>
2022-02-26 12:44:44 +0700

    fix eslint issue

--- apps/access_logger/src/application.ts
+++ apps/access_logger/src/application.ts
@@ -4,7 +4,7 @@ import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
-  RestExplorerComponent
+  RestExplorerComponent,
 } from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
 import {
@@ -13,7 +13,7 @@ import {
   QueueComponent,
   RabbitmqBindings,
   RabbitmqComponent,
-  RabbitmqComponentConfig
+  RabbitmqComponentConfig,
 } from 'loopback-rabbitmq';
 import path from 'path';
 import {MySequence} from './sequence';
